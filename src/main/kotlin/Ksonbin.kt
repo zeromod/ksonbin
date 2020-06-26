9d211e5a5a999cf9198b8dec43fb2dd1fd6a5924
parthasarathy <parthasarathy@logicsoft.co.in>
2020-06-26 18:48:20 +0530

    [design]: bring api into Ksonbin scope

--- src/main/kotlin/Ksonbin.kt
+++ src/main/kotlin/Ksonbin.kt
@@ -1,5 +1,13 @@
 package `in`.zeromod.ksonbin
 
+import `in`.zeromod.ksonbin.api.BinApi
+import `in`.zeromod.ksonbin.api.BinCreateResponse
+import `in`.zeromod.ksonbin.retrofit.API
+import `in`.zeromod.ksonbin.retrofit.ApiResult
+import `in`.zeromod.ksonbin.retrofit.apiCall
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+
 object Ksonbin {
     var root: String = "https://api.jsonbin.io/"
     lateinit var key: String
@@ -7,5 +15,14 @@ object Ksonbin {
 
     fun init(secretKey: String) {
         key = secretKey
+    }
+
+    object Bin {
+        private val binApi: BinApi = API.create(root)
+        suspend fun create(data: String): ApiResult<BinCreateResponse> = apiCall {
+            binApi.create(
+                data.toRequestBody("application/json; charset=utf-8".toMediaType())
+            )
+        }
     }
 }
--- src/main/kotlin/api/Bin.kt
+++ src/main/kotlin/api/Bin.kt
@@ -1,26 +1,12 @@
 package `in`.zeromod.ksonbin.api
 
 import `in`.zeromod.ksonbin.Ksonbin
-import `in`.zeromod.ksonbin.retrofit.API
-import `in`.zeromod.ksonbin.retrofit.ApiResult
-import `in`.zeromod.ksonbin.retrofit.apiCall
 import com.squareup.moshi.Json
-import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody
-import okhttp3.RequestBody.Companion.toRequestBody
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.Header
 import retrofit2.http.POST
-
-object Bin {
-    private val binApi: BinApi = API.create(Ksonbin.root)
-    suspend fun create(data: String): ApiResult<BinCreateResponse> = apiCall {
-        binApi.create(
-            data.toRequestBody("application/json; charset=utf-8".toMediaType())
-        )
-    }
-}
 
 interface BinApi {
     @POST("/b")
